d00fee223fb17a04c89c74fc2c256426f65000c6
Sophie Bauder <38045114+sophiebaud11@users.noreply.github.com>
2021-11-22 12:44:48 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -58,3 +58,13 @@ const runAnimations = () => {
   const countupEls = document.querySelectorAll( '.countup' );
   countupEls.forEach( animateCountUp );
 };
+
+function highlighter(text_to_highlight) {
+	text_to_highlight.style.backgroundPosition = "-99.99% 0";
+}
+function runHighlights() {
+	var highlight1 = document.querySelector("#highlight1");
+	var highlight2 = document.querySelector("#highlight2");
+	setTimeout(highlighter, 1000, highlight1)
+	setTimeout(highlighter, 5000, highlight2)
+}
